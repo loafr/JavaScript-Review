e81d11551b43f371cad6021b784dfcf5477db175
lrobins <lrobins@adobe.com>
2015-01-24 12:18:08 -0700

    completed this object review

--- objectReview.js
+++ objectReview.js
@@ -63,23 +63,28 @@ user.pwHash= "f+L7lngx2B1Qk"
 console.log(user);
 
 
-
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
 //create an empty object called methodCollection. 
 
-  //Your code here
+var methodCollection = {};
 
 //Now add two methods (functions that are properties on objects) to your methodCollection object. 
 //One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
-  //Your code here
+methodCollection.alertHello = function(){
+  alert("Hello");
+}
+methodCollection.logHello = function() {
+  console.log("Hello");
+}
 
 //Now call your alertHello and logHello methods. 
 
-  //Your code here
+methodCollection.alertHello();
+methodCollection.logHello();
 
 
 
@@ -90,6 +95,37 @@ console.log(user);
 //write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, 
 //and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
+var someString = "this is a test for my new function i just created";
+
+var voweler = function(string) {
+  var aCount = 0;
+  var eCount = 0;
+  var iCount = 0;
+  var oCount = 0;
+  var uCount = 0;
+  var newObj = {};
+  var arr = string.split("");
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i] === "a") {
+      newObj.a = aCount++;
+    } else if (arr[i] === "e") {
+        newObj.e = eCount++;
+    } else if (arr[i] === "i") {
+        newObj.i = iCount++;
+    } else if (arr[i] === "o") {
+        newObj.o = oCount++;
+    } else if (arr[i] === "u") {
+        newObj.u = uCount++;
+    } else {
+      // do nothing
+    }
+  }
+    return newObj;
+};
+
+
+
+
 
 
 
