281db043eadaf635c28d6f5f7e0fd05f22695208
lrobins <lrobins@adobe.com>
2015-01-24 10:30:22 -0700

    completed a few more, up to array clone/copy

--- arrayReview.js
+++ arrayReview.js
@@ -28,24 +28,39 @@ var oddRemover = function() {
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. 
+//Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. 
+//If it is, alert true, if it's not, alert false
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+var randomChecker = function(arr) {
+  var ranNum = getRandomArbitrary();
+  for (var i in randomArray)
+    if (arr[i] === ranNum) {
+    	console.log(ranNum);
+    	return alert(true);
+  	} 
+  	console.log(ranNum);
+  	return alert(false);
+  };
 
+
+randomChecker(randomArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
+//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. 
+//When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
+second = first.slice(0)
+second.push(6,7)
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
